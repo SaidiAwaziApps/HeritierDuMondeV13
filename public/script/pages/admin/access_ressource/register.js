b56c38687c3abf19e5c6d0f0ad3f683141574699
Saidi Awazi <saidiawaziapps@gmail.com>
2026-04-19 15:13:31 +0200

    Mise a jour des fichiers script pour access_ressources;

---
+++ public/script/pages/admin/access_ressource/register.js
@@ -0,0 +1,22 @@
+/* **************************************************************
+ * LORSQUE LE DOM EST CHARGE
+ * *************************************************************/
+document.addEventListener('DOMContentLoaded', function() {
+    /******** VARIABLES DOM ********/
+    const check_all_input = document.querySelector('input[id="check_all"]');
+    const elements = document.querySelectorAll('input[id="access_ressources"]');
+
+    /******* EN CAS DE CLICK SUR LA CASE ******/
+    check_all_input.onclick = function(){    
+        if(this.checked == true){
+            elements.forEach(item => {
+                item.checked = true;   
+            });        
+        } else {
+            elements.forEach(item => {
+                item.checked = false;   
+            }); 
+        }
+    }
+});
+
---
+++ public/script/pages/admin/access_ressource/update.js
@@ -0,0 +1,30 @@
+/* **************************************************************
+ * LORSQUE LE DOM EST CHARGE
+ * *************************************************************/
+document.addEventListener('DOMContentLoaded',function() {
+    /* ---- Variables DOM ---- */
+    let elements = document.querySelectorAll('input[type="checkbox"]');
+    let check_all_input = document.querySelector('input[id="check_all"]');
+
+    /* ---- Parcourt ensembles du tableau access_ressource && elements ---- */
+    access_ressources.forEach(access_ressource => {
+        elements.forEach(element => {
+            if(access_ressource.ressource.id == parseInt(element.value.split(',')[0]) && access_ressource.action==element.value.split(',')[1] && access_ressource.mention.toLowerCase()=='allowed') {
+                element.checked = true; 
+            }
+        });
+    });
+
+    /* ---- Au moment du clic sur un des elements inputs ---- */
+    check_all_input.onclick = function(e){
+        if(e.currentTarget.checked == true){
+            elements.forEach(item => {
+                item.checked = true;   
+            });        
+        } else {
+            elements.forEach(item => {
+                item.checked = false;   
+            }); 
+        }
+    }
+});
