0985beca90ec4842979b2e41939004f2a3a9aca5
Saidi Awazi <saidiawaziapps@gmail.com>
2026-04-19 17:28:27 +0200

    Mise a jour de la methode getOne

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
     {
         return self::where('id', $id)
                    ->where('status', true)
-                   ->first();  
+                   ->firstOrFail();  
     }
 
     /**
