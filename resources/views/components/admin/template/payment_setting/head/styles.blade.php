e579cba1fe442eb182e7a9756d6a26e62b2ac122
Saidi Awazi <saidiawaziapps@gmail.com>
2026-04-20 08:45:42 +0200

    Mise en place du fichier style template head pour payment_setting

---
+++ resources/views/components/admin/template/payment_setting/head/styles.blade.php
@@ -0,0 +1,8 @@
+@if(Route::currentRouteName() == 'paymentSetting.register_page')
+   <link rel="stylesheet" href="{{ asset('style/pages/admin/payment_setting/register.css') }}">
+@endif
+
+@if(Route::currentRouteName() == 'paymentSetting.update_page')
+   <link rel="stylesheet" href="{{ asset('style/pages/admin/payment_setting/update.css') }}">
+@endif
+
