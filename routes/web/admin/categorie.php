369085d0c408d40e6e65f3d871c4d1613fe86498
Saidi Awazi <saidiawaziapps@gmail.com>
2026-05-01 16:07:28 +0300

    Mise a jour du fichier de routing categorie avec modification du model de chargement de middlware

--- routes/web/admin/categorie.php
+++ routes/web/admin/categorie.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\Admin\CategorieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,12 +10,13 @@ use App\Http\Controllers\CategorieController;
 */
 
 Route::prefix('categorie')
-    ->middleware(['categorie.ressource.global','trackHistoryMiddleware'])
+    ->as('categorie.')
+    ->middleware([\App\Http\Middleware\IsAuthenticate::class])
     ->group(function() {
 
         // Enregistrement d'une catégorie
         Route::post('/register', [CategorieController::class, 'save'])
-            ->name('categorie.save');
+            ->name('save');
 
         // Ici tu peux ajouter d'autres routes : list, update, delete, details, etc.
 });
