747140beb6a449019cd2077041f8e81d133d5575
Saidi Awazi <saidiawaziapps@gmail.com>
2026-05-01 15:59:29 +0300

    Change le nom du fichier admin_template_items en items

---
+++ resources/views/components/admin/template/blog/article/head/scripts.blade.php
@@ -0,0 +1,5 @@
+
+@if(Route::currentRouteName() == 'admin.article.register_page' || Route::currentRouteName() == 'admin.article.update_page')
+    <script src="{{ asset('dependance/axios/axios.min.js') }}"></script>
+    <script src="https://cdn.tiny.cloud/1/ee38iugviofz8h4oh6zztci4avjx4mkq8bq6k73l9mru5ej9/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
+@endif
