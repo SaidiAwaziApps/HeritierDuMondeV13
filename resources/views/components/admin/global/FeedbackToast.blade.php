e789cd0886d805882bccefba66ee09e40f40ed48
Saidi Awazi <saidiawaziapps@gmail.com>
2026-05-01 16:31:23 +0300

    Mise en place du component global FeedbackToast

---
+++ resources/views/components/admin/global/FeedbackToast.blade.php
@@ -0,0 +1,10 @@
+<!-- Container pour positionner les toasts -->
+<div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+  <div id="feedbackToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-body">
+        <div id="feedbackToastContent">
+             
+        </div>
+    </div>
+  </div>
+</div>
