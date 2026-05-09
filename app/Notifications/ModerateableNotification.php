7a79bb5c4c4a8a3f8a39aad7a3bcf12c15f00b48
Saidi Awazi <saidiawaziapps@gmail.com>
2026-05-09 15:24:20 +0300

    Mise a jour de la notification ModerateableNotification

--- app/Notifications/ModerateableNotification.php
+++ app/Notifications/ModerateableNotification.php
@@ -3,10 +3,11 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\BroadcastMessage;
 
-class ModerateableNotification extends Notification
+class ModerateableNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
