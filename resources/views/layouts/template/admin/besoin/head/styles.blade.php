a26355c554c24313d1fa46912dc16884eb763f80
Saidi Awazi <saidiawaziapps@gmail.com>
2026-05-11 01:19:56 +0300

    Mise en place du template layout pour besoin

---
+++ resources/views/layouts/template/admin/besoin/head/styles.blade.php
@@ -0,0 +1,21 @@
+    @if(Route::currentRouteName() == 'admin.besoin.register_page' || Route::currentRouteName() == 'admin.besoin.update_page')
+        <link rel="stylesheet" href="{{ asset('style/components/admin/image/global/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('style/components/admin/image/upload/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('style/components/admin/image/vignette/style.css') }}">    
+    @endif    
+
+    @if(Route::currentRouteName() == 'admin.besoin.register_page')
+        <link rel="stylesheet" href="{{ asset('style/pages/admin/besoin/register.css') }}">
+    @endif
+
+    @if(Route::currentRouteName() == 'admin.besoin.update_page')
+        <link rel="stylesheet" href="{{ asset('style/pages/admin/besoin/update.css') }}">
+    @endif
+
+    @if(Route::currentRouteName() == 'admin.besoin.list')
+        <link rel="stylesheet" href="{{ asset('style/pages/admin/besoin/list.css') }}">
+    @endif
+
+    @if(Route::currentRouteName() == 'admin.besoin.details')
+        <link rel="stylesheet" href="{{ asset('style/pages/admin/besoin/details.css') }}">
+    @endif
