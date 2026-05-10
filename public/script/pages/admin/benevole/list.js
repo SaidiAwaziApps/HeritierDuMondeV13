9b880e0c0ed71df35f5e00a43ace9081d1e9f607
Saidi Awazi <saidiawaziapps@gmail.com>
2026-05-10 17:16:56 +0300

    Mise en place du script pour benevole

---
+++ public/script/pages/admin/benevole/list.js
@@ -0,0 +1,16 @@
+
+// document.onreadystatechange=function(){
+//     if(document.readyState.toLowerCase()=='complete') {
+//         const data_items = document.querySelectorAll('div[id="data_item"]');
+//         const data_more_options = document.querySelectorAll('div[id="data_more_option"]');
+//         data_items.forEach(function(item,index){
+//             data_more_options[index].style.width = item.innerWidth;
+//             data_more_options[index].style.height=item.innerHeight;
+//             item.style.height='100%';
+//             data_more_options[index].style.top='-100%';
+//         });
+//     }
+// }
+
+
+
