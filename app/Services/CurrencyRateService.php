d680fe16d095fa9f44028ed5d51651fb171dfa86
Saidi Awazi <saidiawaziapps@gmail.com>
2026-06-25 12:58:49 +0300

    Mise a jour du service CurrencyRateService avec adoption du retour du recent taux d'echange de la base donnee

--- app/Services/CurrencyRateService.php
+++ app/Services/CurrencyRateService.php
@@ -5,12 +5,17 @@ namespace App\Services;
 use Illuminate\Support\Facades\Http;
 use Exception;
 
+use App\Models\PaymentSetting;
+
 class CurrencyRateService
 {
     public static function getRate()
     {
         $from = 'USD';
         $to = 'EUR';
+
+        // Instance payment setting
+        $paymentSetting = PaymentSetting::findOrFail(1);
 
         try {
             $response = Http::timeout(5)->get('https://api.frankfurter.app/latest', [
@@ -22,16 +27,23 @@ class CurrencyRateService
                 logger()->error('CurrencyRateService API error', [
                     'status' => $response->status(),
                 ]);
-
-                return null;
+           
+                 // Renvoie le taux d' echange recent 
+                return $paymentSetting->last_currency_rate;
             }
+
+            // Modifie le taux d' change courant de l'instance
+            $paymentSetting->update([
+               'last_currency_rate' => $response->json("rates.$to")
+            ]);
 
             return $response->json("rates.$to");
 
         } catch (Exception $e) {
             logger()->error("CurrencyRateService exception: " . $e->getMessage());
 
-            return null;
+            // Renvoie le taux d' echange recent 
+            return $paymentSetting->last_currency_rate;
         }
     }
 }
