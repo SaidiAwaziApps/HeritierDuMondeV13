82f42497fb5e542457ab7aec1dec8353193dca8a
Saidi Awazi <saidiawaziapps@gmail.com>
2026-07-09 17:18:21 +0300

    Ajout ajout le prefixe /admin a l'ensemble des routes destinees a user

--- routes/web/admin/user.php
+++ routes/web/admin/user.php
@@ -1,22 +1,25 @@
 <?php
+use App\Http\Middleware\IsAdmin;
+use App\Http\Middleware\TrackHistoryMiddleware;
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserController;
 
-Route::middleware([\App\Http\Middleware\IsAdmin::class,\App\Http\Middleware\TrackHistoryMiddleware::class])
+
+Route::middleware(IsAdmin::class)
     ->prefix('admin/user')
     ->as('admin.user.')
     ->group(function() {
 
-        Route::get('/register', [UserController::class, 'register_page'])->name('register_page');
-        Route::get('/list', [UserController::class, 'list'])->name('list');
-        Route::get('/details/{id}', [UserController::class, 'details'])->name('details');
-        Route::get('/update/{id}', [UserController::class, 'update_page'])->name('update_page');
-        Route::get('/reset-password/{id}', [UserController::class, 'reset_password_page'])->name('reset_password_page');
+        Route::get('/register', [UserController::class, 'register_page'])->middleware(TrackHistoryMiddleware::class)->name('register_page');
+        Route::get('/list', [UserController::class, 'list'])->middleware(TrackHistoryMiddleware::class)->name('list');
+        Route::get('/details/{id}', [UserController::class, 'details'])->middleware(TrackHistoryMiddleware::class)->name('details');
+        Route::get('/update/{id}', [UserController::class, 'update_page'])->middleware(TrackHistoryMiddleware::class)->name('update_page');
+        Route::get('/reset-password/{id}', [UserController::class, 'reset_password_page'])->middleware(TrackHistoryMiddleware::class)->name('reset_password_page');
 
         // Cette route nécessite un middleware supplémentaire
         Route::get('/my-profil', [UserController::class, 'my_profil'])
-            ->middleware(\App\Http\Middleware\IsHighAdmin::class)
+            ->middleware(TrackHistoryMiddleware::class)
             ->name('my_profil');
 
         Route::post('/save', [UserController::class, 'save'])->name('save');
