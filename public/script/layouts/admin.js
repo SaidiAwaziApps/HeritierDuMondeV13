233501888c78ffae8b94979c4dcd7f8f8a908aaf
Saidi Awazi <saidiawaziapps@gmail.com>
2026-07-22 17:06:00 +0300

    Mise au point d'un script commun a tous les menus comprenant de sous-menus (parametres && offres)

--- public/script/layouts/admin.js
+++ public/script/layouts/admin.js
@@ -16,24 +16,25 @@ function toggleSidebar(evenement){
 
 
 /* ******************************************************************* 
- * METHODE SERVANT D' AFFICHER OU CACHANT UN SOUS MENU PARAMETRE
+ * METHODE SERVANT D' AFFICHER OU CACHANT UN SOUS MENU 
  * *******************************************************************/
-function toggleParameterSubMenu(parameterMenuLink, parameterSubMenu) {
+function toggleSubMenu(menuLink, subMenu) {
     // Sous-menu visible
-    if(parameterSubMenu.style.display == 'block') {
-        parameterMenuLink.querySelectorAll('i')[1].setAttribute('class','fa fa-angle-down');
-        parameterSubMenu.style.display = 'none';
+    if(subMenu.style.display == 'block') {
+        menuLink.querySelectorAll('i')[1].setAttribute('class','fa fa-angle-down');
+        subMenu.style.display = 'none';
     } else {
-        parameterMenuLink.querySelectorAll('i')[1].setAttribute('class','fa fa-angle-up');
-        parameterSubMenu.style.display = 'block';
+        menuLink.querySelectorAll('i')[1].setAttribute('class','fa fa-angle-up');
+        subMenu.style.display = 'block';
     }
 }
+
 
 
 /* ******************************************************************
  * METHODE QUI AFFICHE LE MESSAGE DE MODERATION DE COMMENTAIRE
  * ******************************************************************/
-function modrationMessage(notification) {
+function showModrationMessage(notification) {
     setTimeout(() => {
         Swal.fire({
         title: 'Moderation de commentaire',
@@ -53,44 +54,49 @@ function modrationMessage(notification) {
     },2000);
 }
 
+
 /* *****************************************************************
  * AU MOMENT DU CHARGEMENT DU DOM
  * *****************************************************************/
 document.addEventListener('DOMContentLoaded',function() {
-    /************* Variables DOM ************/
+    /* ---- Variables DOM --- */
     const collapseButton = document.querySelector('button[id="collapse_button"]');
     const dismissButton = document.querySelector('button[id="dismiss_button"]');
-    
-    const parameterMenuLink = document.querySelector('a[id="parameter_menu_link"]');
-    const parameterSubMenu = document.querySelector('ul[id="parameter_sous_menu"]');
 
-    /******* Cache le sous menu a l' initiale *******/
-    parameterSubMenu.style.display = 'none';
+    const menuLinks = document.querySelectorAll('a[id="parameter_menu_link"], a[id="offer_menu_link"]');
+    const subMenus = document.querySelectorAll('ul[id="parameter_sous_menu"], ul[id="offer_sous_menu"]');
 
-    /********** Clique sur le button collapse *********/
+    /* ---- Cache le sous menu a l' initiale ---- */
+    subMenus.forEach(item => {
+        item.style.display = 'none';  
+    });
+
+    /* ---- Clique sur le button collapse ---- */
     collapseButton.onclick = function(){
         toggleSidebar('collapse');    
     }  
 
-    /********** Clique sur le button dismiss *********/
+    /* ---- Clique sur le button dismiss ---- */
     dismissButton.onclick = function(){
         toggleSidebar('dismiss');    
     }
     
-    /********** Clique sur le lien parametre-menu *********/
-    parameterMenuLink.onclick = function(e) {
-        e.preventDefault();
-        // Appel a la methode function
-        toggleParameterSubMenu(parameterMenuLink, parameterSubMenu)
-    }
+    /* ---- Clique sur le lien parametre-menu ---- */
+    menuLinks.forEach((item,index) => {
+        item.onclick = (e) => {
+            e.preventDefault();
+            // Appel a la methode toggleSubMenu
+            toggleSubMenu(item,subMenus[index]);
+        }     
+    });
 
-    /********** Ecouteur d'evenement (Pusher) ***********/       
+    /* ---- Ecouteur d'evenement (Pusher) ---- */       
     window.Echo
         .private('App.Models.User.'+window.user.id)
         .notification((notification) => {
             // Notification pour moderation && page different de details pour blog article
             if(notification?.moderateable && window.currentRouteName != 'admin.article.details') {
-                modrationMessage(notification); 
+                showModerationMessage(notification); 
             }  
         });
 });
